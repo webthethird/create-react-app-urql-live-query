6a8b6f443feacd1f33ebe4b67f0b665d8c4977e3
webthethird <ratzbodell@gmail.com>
2024-05-21 14:05:03 -0400

    Add query usage in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { formatEther } from "ethers/lib/utils";
+import logo from "./logo.svg";
+import { usePastDayHourlyCandlesLiveQuery } from "./graphql/generated";
+import "./App.css";
+
+interface GenericCandle {
+  openTimestamp: string;
+  high: string;
+  low: string;
+  open: string;
+  close: string;
+  volume: string;
+}
+
+const formatShortDollarValue = (value: number): string => {
+  if (value >= 10 ** 9)
+    return `${value >= 0 ? "" : "-"}$${Math.abs(
+      Math.floor(value / 10 ** 7) / 100
+    ).toFixed(2)}b`;
+  if (value >= 10 ** 6)
+    return `${value >= 0 ? "" : "-"}$${Math.abs(
+      Math.floor(value / 10 ** 4) / 100
+    ).toFixed(2)}m`;
+  if (value >= 10 ** 3)
+    return `${value >= 0 ? "" : "-"}$${Math.abs(
+      Math.floor(value / 10 ** 1) / 100
+    ).toFixed(2)}k`;
+  return `${value >= 0 ? "" : "-"}$${Math.abs(value).toFixed(2)}`;
+};
+
+const formatMarketVolume = (
+  candles: GenericCandle[] | undefined
+): string | undefined =>
+  formatShortDollarValue(
+    candles?.reduce(
+      (acc, val) => acc + Math.abs(Number(formatEther(val.volume))),
+      0
+    ) ?? 0
+  );
 
 function App() {
+  const [hourlyCandlesLiveQuery] = usePastDayHourlyCandlesLiveQuery({
+    variables: {
+      marketId: "0",
+      from: Math.round(Date.now().valueOf() / 1000) - 24 * 60 * 60,
+    },
+  });
+
+  const volume = React.useMemo(
+    () =>
+      formatMarketVolume(
+        hourlyCandlesLiveQuery?.data?.market?.hourlyCandles as
+          | GenericCandle[]
+          | undefined
+      ),
+    [hourlyCandlesLiveQuery]
+  );
+
+  console.log(hourlyCandlesLiveQuery.data?.market?.hourlyCandles);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Market Volume: {volume} {volume ? "USD" : ""}
+        </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
